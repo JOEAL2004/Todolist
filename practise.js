30ff82d12f2ecb4b0fc498a770855d8b66d9fff6
JOEAL2004 <joealjames610@gmail.com>
2026-07-03 07:29:55 +0530

    Add javascript datatype practise

---
+++ practise.js
@@ -0,0 +1,11 @@
+let name = "Joeal";
+let age = 21;
+let student = true;
+let score;
+let country = null;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof student);
+console.log(typeof score);
+console.log(typeof country);
